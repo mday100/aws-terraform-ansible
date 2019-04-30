8da0af905329d961a34167baa0967315712fa32e
Matt Day <matt@unlimitedday.com>
2019-04-30 04:52:47 -0400

    start scripting the vpc

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 
 # ---------- IAM ----------
 
-# S3_ACCESS
+# S3 ACCESS
 
 resource "aws_iam_instance_profile" "s3_access_profile" {
   name = "s3_access"
@@ -49,3 +49,45 @@ resource "aws_iam_role" "s3_access_role" {
 }
 EOF
 }
+
+# ---------- VPC ----------
+
+resource "aws_vpc" "wp_vpc" {
+  cidr_block = "${var.vpc_cidr}"
+  enable_dns_hostnames = true
+  enable_dns_support = true
+
+  tags {
+    Name = "wp_vpc"
+  }
+}
+
+# INTERNET GATEWAY
+
+resource "aws_internet_gateway" "wp_internet_gateway" {
+  vpc_id = "${aws_vpc.wp_vpc.id}"
+
+  tags {
+    Name = "wp_igw"
+  }
+}
+
+# ROUTE TABLES
+
+resource "aws_route_table" "wp_public_rt" {
+  vpc_id = "${aws_vpc.wp_vpc.id}"
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.wp_internet_gateway.id}"
+  }
+}
+
+resource "aws_default_route_table" "wp_private_rt" {
+  default_route_table_id = "${aws_vpc.wp_vpc.default_route_table_id}"
+
+  tags {
+    Name = "wp_private"
+  }
+}
+
